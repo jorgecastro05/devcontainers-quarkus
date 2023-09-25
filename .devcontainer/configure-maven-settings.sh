8a3d05c44bb70eadd181af39691bc434f25ba7ab
Jorge Castro <jorgecastro05@hotmail.com>
2023-09-24 20:31:00 -0500

    Update configure-maven-settings.sh

--- .devcontainer/configure-maven-settings.sh
+++ .devcontainer/configure-maven-settings.sh
@@ -1,3 +1,6 @@
 echo "configuring maven settings"
 mkdir -p /home/vscode/.m2
 cp .devcontainer/maven-settings.xml /home/vscode/.m2/settings.xml
+
+echo "set mandrel as default java distro"
+sdk default java 23.r17-mandrel
